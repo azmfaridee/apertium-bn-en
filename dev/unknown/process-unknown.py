c31ad640dbaaa23bfa57161ac113e01682057d86
darthxaher <zaher14@gmail.com>
2009-07-02 08:53:19 +0000

    Unknown List
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@12253 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ dev/unknown/process-unknown.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.5
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import os
+import sys
+import re
+
+alpha = '১২৩৪৫৬৭৮৯০অআইঈউঊঋএঐওঔকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহড়ঢ়য়ঁািীুূৃেৈোৌংঃ্'
+word = '[' + alpha + ']+'
+regex = '(\^)(' + word + ')(\/\*)(' + word + ')(\$)'
+p = re.compile(regex)
+words = []
+
+file = open('unknown.txt')
+for line in file:
+    words.append(p.search(line).group(2))
+file.close()
+
+for word in words:
+    print word
