f288c7cf8e1d684bc4b317d749721f1b9eb52d31
darthxaher <zaher14@gmail.com>
2009-08-08 15:36:13 +0000

    Adverb Fix
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@13809 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/adverb/create-adverb-dix.sh
+++ dev/adverb/create-adverb-dix.sh
@@ -5,6 +5,8 @@
 #../paradigm-chopper.py /tmp/adverb.bn.dix > adverb.bn.dix
 
 # create speling from the list file, not the database
+# run the prune script the remove the duplicates
+cat adverb-extra.bn.list adverb-main.bn.list | ../prune.py > adverb.bn.list
 cat ./adverb.bn.list | awk '{print $0"; "$0"; ;adv"}' > ./adverb.bn.speling
 
 # quick dix
