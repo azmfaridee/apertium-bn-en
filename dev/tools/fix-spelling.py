cd3ef21a07c2fe7350df5e495ad69330bc003e97
darthxaher <zaher14@gmail.com>
2010-01-01 17:24:43 +0000

    minor change in fix-spelling.py
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@18266 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/tools/fix-spelling.py
+++ dev/tools/fix-spelling.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python
 
+# Program info:
+# use this program to fix common speling errors in bengali text
+# ড় is sometimes written as ড + nukta (unicode value 09bc), we want to
+# represent this with one character
+# same is true for ঢ় and য় and ৎ (at least at the word ending section)
+
+# Input: stdin, each line contains one Bengali word
+# Output: stdout, each line contains one fixed Bengali word
+
 import sys, codecs
 
 words = []
-for x in sys.stdin: words.append(x.strip())
-for index, value in enumerate(words):
-  word = words[index].decode('utf-8')
+
+# load the data from stdin from buffer, sometime direct processing creates some
+# problems
+for x in sys.stdin:
+  words.append(x.strip())
+  
+for word in words:
+  # decoding is important
+  word = word.decode('utf-8')
+  
   # replace ত্  at the end with ৎ
   if word[-2:] == u'\u09a4\u09cd':  word = word[:-2] + u'\u09ce'
+  
   # replacement for ড়, ঢ় and য় which ara written with nukta
   word = word.replace(u'\u09a1\u09bc', u'\u09dc').replace(u'\u09a2\u09bc', u'\u09dd').replace(u'\u09af\u09bc', u'\u09df')
+  
+  # output
   print >> sys.stdout, word
