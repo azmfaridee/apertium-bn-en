eae3f72687f2f2d7763aa4283db7f58e60d3c368
darthxaher <zaher14@gmail.com>
2010-01-03 03:35:24 +0000

    svn (diff) friendly db backup and restore script
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@18285 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/db-backup/restore-db-backup.sh
+++ dev/db-backup/restore-db-backup.sh
@@ -2,7 +2,9 @@
 
 echo "DROP DATABASE if exists bengali_conjugator; CREATE DATABASE bengali_conjugator DEFAULT CHARACTER SET utf8 COLLATE utf8_bangla_ci; use bengali_conjugator;" | mysql mysql -uroot -proot
 
-bzcat $1 | mysql bengali_conjugator -uroot -proot
+# we are not using bzipped tar files for backup anymore
+# bzcat $1 | mysql bengali_conjugator -uroot -proot
+cat mysqldump.sql | mysql bengali_conjugator -uroot -proot
 
 echo 'Press any key to continue ...'
 read dummy
--- dev/db-backup/take-db-backup.sh
+++ dev/db-backup/take-db-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mysqldump -uroot -proot --routines bengali_conjugator | bzip2 > bengali_conjugator$(date +_%Y%m%d_%H%M).sql.bz2
+# we are not using bzipped tar files for backup anymore
+# mysqldump -uroot -proot --routines bengali_conjugator | bzip2 > bengali_conjugator$(date +_%Y%m%d_%H%M).sql.bz2
+mysqldump -uroot -proot --routines bengali_conjugator | mysqldump.sql
 
 echo 'Press any key to continue ...'
 read dummy
