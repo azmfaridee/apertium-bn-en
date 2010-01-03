586887e9e1c1dcb4849854b3c426401b69a797da
darthxaher <zaher14@gmail.com>
2010-01-03 04:11:50 +0000

    mysqldump 20100103-1010 and take-db-backup.sh fixed
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@18298 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/db-backup/take-db-backup.sh
+++ dev/db-backup/take-db-backup.sh
@@ -2,7 +2,7 @@
 
 # we are not using bzipped tar files for backup anymore
 # mysqldump -uroot -proot --routines bengali_conjugator | bzip2 > bengali_conjugator$(date +_%Y%m%d_%H%M).sql.bz2
-mysqldump -uroot -proot --routines bengali_conjugator | mysqldump.sql
+mysqldump -uroot -proot --routines bengali_conjugator | python make_svn_fiendly_mysql_dump.py > mysqldump.sql
 
 echo 'Press any key to continue ...'
 read dummy
