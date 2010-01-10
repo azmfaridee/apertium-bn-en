b5ec7fb717fb8a402492892468952b96ffefe408
darthxaher <zaher14@gmail.com>
2010-01-10 16:57:41 +0000

    new file:   split-expand.py, splits the expand files into multiple parts, each file for each specific lemma
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/trunk/apertium-bn-en@18524 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ dev/tools/split-expand.py
@@ -0,0 +1,49 @@
+﻿#!/usr/bin/python
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys
+import codecs
+import os
+
+if __name__ == '__main__':
+	# FIXME: The follwing line conflicts with raw_input :(
+	# sys.stdin = codecs.getreader('utf-8')(sys.stdin)
+	sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+	sys.stderr = codecs.getwriter('utf-8')(sys.stderr)
+	
+	if len(sys.argv) < 3:
+		print 'python split-expand.py <input.expand> <output-dir>'
+		print 'e.g. python split-expand.py vblex.expand verb-expand-folder'
+		sys.exit(1)
+	
+	response = raw_input('This script will remove all files in \'' + sys.argv[2] + '\'. Do you want to continue (y/N)?: ')
+	if response.strip() != 'Y' and response.strip() != 'y':
+		sys.exit(1)
+	print 'Deleting all expand files in ', sys.argv[2]
+	os.system('rm -f '+ sys.argv[2] + '/*.expand ' +  sys.argv[2] +'/filelist.txt')
+	
+	groups = {}
+	print 'Reading source file'
+	with codecs.open(sys.argv[1], 'r', 'utf-8') as file:
+		# lines = map(lambda x: x.strip(), file.readlines())
+		lines = file.readlines()
+	
+	for line in lines:
+		lemma = line[line.index(':')+1:line.index('<')]
+		pos = line[line.index('<')+1:line.index('>')]
+		id = lemma + ':' + pos
+		if id not in groups.keys(): groups[id] = []
+		else: groups[id].append(line)
+	
+	filelist = []
+	i = 0
+	print 'Writing output files'
+	for id in groups.keys():
+		filename = sys.argv[2].strip('/') + '/' + str(i) + '.expand'
+		filelist.append(filename + ' -> ' + id + '\n')
+		with codecs.open(filename,'w', 'utf-8') as file:
+			file.writelines(groups[id])
+		i += 1
+	with codecs.open(sys.argv[2].strip('/') + '/' + 'filelist.txt', 'w', 'utf-8') as file:
+		file.writelines(filelist)
