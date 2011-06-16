aa1fb30189b949153b82779e2101c0757090f3bd
ragib06 <ragib.ahsan@gmail.com>
2011-06-16 18:30:12 +0000

    144 nouns added to monodix with some associating scripts
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/incubator/apertium-bn-en@30731 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ dev/gsoc2011/monodix/noun/create_noun_monodix_entry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, re
+
+table = {}
+tag = {}
+template = "    <e lm=\"xxx\"><i>xxx</i><par n=\"yyy__n_|\"/></e>"
+
+#load adjective pardef table
+with open('noun.pardefs','r') as f:
+	for line in f:
+		if line == '\n':
+			continue
+		
+		pd = line.strip().split(',')
+		par = pd[1].split()
+		table[par[0]] = pd[0]
+		tgs = par[1].split('>')
+		tag[par[0]] = re.sub('<','',tgs[0])
+
+#process new words
+for i in sys.stdin:
+	line = i.strip().split(',')
+	temp = re.sub('yyy',table[line[1]],re.sub('xxx',line[0],template))
+	t = temp.split('|')
+	print t[0] + tag[line[1]] + t[1]
---
+++ dev/gsoc2011/monodix/noun/tag-noun.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#backup previous copy of "noun.todo"
+cat fresh.noun | cat > fresh.noun_back
+echo "--------------"
+
+
+#tag
+n=`grep -c "" noun.bn.patch`
+if [ $n -gt 0 ]; then
+	cat fresh.noun | grep -v "\!" | ./create_noun_monodix_entry.py > /tmp/bar6
+fi
+
+#append to patch fle
+if [ -f noun.bn.patch ]; then
+	noe_=`grep -c "" noun.bn.patch`
+else
+	noe_=`expr 0`
+fi
+
+noe=`grep -c "" /tmp/bar6`
+
+if [ $noe -gt 0 ]; then
+	echo "    <!-- nouns added @ "`date --utc`" -->" >> noun.bn.patch
+	cat /tmp/bar6 | cat >> noun.bn.patch
+	noe_=`expr $noe_ + 1`
+fi
+
+noe=`grep -c "" noun.bn.patch`
+noe=`expr $noe - $noe_`
+echo "patched  : $noe"
+
+#remove from todo file
+cat fresh.noun | grep '\!' > /tmp/bar6
+cat /tmp/bar6 > fresh.noun
+
+noe=`grep -c "" fresh.noun`
+echo "remaining: $noe"
+echo "--------------"
